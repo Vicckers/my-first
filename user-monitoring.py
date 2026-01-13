c726db091a153685545a71fdac254a605c926fda
hevoryx <160888378+Vicckers@users.noreply.github.com>
2026-01-14 00:35:23 +0500

    Update user-monitoring.py

--- user-monitoring.py
+++ user-monitoring.py
@@ -7,7 +7,7 @@ API_ID = 28314907
 API_HASH = '01bffe95d8af217cf794e66d989c227e'
 PHONE_NUMBER = '+998774490625'
 
-TARGET_USERNAME = 'scalper'
+TARGET_USERNAME = 'algorythm'
 
 
 async def main():
@@ -48,6 +48,7 @@ async def main():
 if __name__ == '__main__':
 
     asyncio.run(main())
+
 
 
 
